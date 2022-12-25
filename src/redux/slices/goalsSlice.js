763277d0291c619c19522625ab93c50c2680210c
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2022-12-25 17:59:34 -0500

    remove hardcoded goals object from goalsSlice
    prepare for appthunk

--- src/redux/slices/goalsSlice.js
+++ src/redux/slices/goalsSlice.js
@@ -1,46 +1,35 @@
-import { createSlice, current, original } from '@reduxjs/toolkit'
-import input from "./inputSlice"
+import {
+  createAsyncThunk,
+  createSlice,
+  current,
+  original,
+} from '@reduxjs/toolkit';
+import input from './inputSlice';
 
-// const addGoal
-
-const initialState = [
-    {
-        id: 1,
-        input: 'drink water',
-        achieved: true
-    },
-    {
-        id: 2,
-        input: 'in bed by 11:00',
-        achieved: true
-        },
-    {
-        id: 3,
-        input: 'study 2hrs',
-        achieved: true
-    },
-]
+const initialState = {
+  goals: [],
+};
 
 export const goalsSlice = createSlice({
   name: 'goals',
   initialState,
   reducers: {
     addGoal: (state, action) => {
-        state.push(action.payload)
-      },
+      state.goals.push(action.payload);
+    },
     deleteGoal: (state, action) => {
-      const goalId = action.payload
-      state = state.filter((goal) => goal.input !== goalId)
+      const goalId = action.payload;
+      state = goals.filter(goal => goal.input !== goalId);
       // console.log(state.map((goal) => goal.input))
       // console.log(current(state))
-      return console.log(state)
+      return console.log(state);
     },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { addGoal, deleteGoal } = goalsSlice.actions
+export const { addGoal, deleteGoal } = goalsSlice.actions;
 
-export default goalsSlice.reducer
+export default goalsSlice.reducer;
 
-      // console.log(goalId)
+// console.log(goalId)
