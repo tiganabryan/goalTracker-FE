c40f644c04f2981ba0d3166b83aba75cb2408592
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2023-01-03 00:32:08 -0500

    ignore

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -32,7 +32,7 @@ const Goals = () => {
       </Text>
 
       {goals?.map((goal, index) => (
-        <Tooltip label="double click to delete" placement="top end">
+        <Tooltip label="double click to delete" placement="top-end">
           <Text
             key={index}
             className="goal-text"
